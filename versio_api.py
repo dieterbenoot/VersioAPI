043e85344db834fb22027f33fd760fd71c7b8c5d
dieterbenoot <dieter@benoot.be>
2021-01-15 23:11:49 +0100

    Working get response

--- versio_api.py
+++ versio_api.py
@@ -1,1 +1,14 @@
-import nothing
+import requests
+from requests.auth import HTTPBasicAuth
+user = 'user'
+password = 'password'
+
+cat = '/dnstemplates'
+
+session = requests.Session()
+session.auth = (user, password)
+hostname = 'https://www.versio.nl/testapi/v1'
+response = session.get(hostname + cat)
+
+
+print(response.json())
